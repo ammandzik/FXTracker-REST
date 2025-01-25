5f232dc6783f8b8a11fa210d470b709ea3b97113
ammandzik <preisner.amanda@gmail.com>
2025-01-25 10:57:42 +0100

    fixing problem with lombok annotations

--- FXTracker/src/main/java/com/FXTracker/advice/ExceptionHandlerApp.java
+++ FXTracker/src/main/java/com/FXTracker/advice/ExceptionHandlerApp.java
@@ -1,9 +1,25 @@
 package com.FXTracker.advice;
 
+import com.FXTracker.exception.StockNotFoundException;
+import com.FXTracker.exception.StockServiceException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionHandlerApp {
+
+    @ExceptionHandler(StockServiceException.class)
+    public String handleException(StockServiceException exception, Model model) {
+
+        return "Sorry for any inconvenience caused. Internal server error occurred.";
+    }
+
+    @ExceptionHandler(StockServiceException.class)
+    public String handleException(StockNotFoundException exception, Model model) {
+
+        return "Given stock was not found.";
+    }
 
 
 }
--- FXTracker/src/main/java/com/FXTracker/controller/StockController.java
+++ FXTracker/src/main/java/com/FXTracker/controller/StockController.java
@@ -1,7 +1,35 @@
 package com.FXTracker.controller;
 
-import org.springframework.stereotype.Controller;
+import com.FXTracker.exception.StockNotFoundException;
+import com.FXTracker.exception.StockServiceException;
+import com.FXTracker.model.StockWrapper;
+import com.FXTracker.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class StockController {
+
+    private final StockService stockService;
+
+    @GetMapping("/stocks/{ticker}")
+    public ResponseEntity<StockWrapper> getStock(@PathVariable String ticker) {
+
+        try {
+            var stock = stockService.findStock(ticker);
+            return ResponseEntity.ok(stock);
+
+        } catch (StockNotFoundException exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
+        } catch (StockServiceException exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+
+    }
 }
